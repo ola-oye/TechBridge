ad2d86980fd2d170d77553f54c1f3a5e12cf902d
ola-oye <olasunkanmioyetunji4@gmail.com>
2024-11-06 19:45:17 +0100

    add logic to return Fizz for even and Buzz for odd

--- D061124/script.js
+++ D061124/script.js
@@ -5,3 +5,12 @@ for (let i = 1; i < 100; i++) {
     }
 }
 console.log(`The number of even number between 1 to 100 is ${counter}`); // The number of even number between 1 to 100 is 50
+
+for (let j=1; j<=100; j++){
+    if(j%2===0){
+        console.log("Fizz");
+    }
+    else{
+        console.log("Buzz");
+    }
+}
