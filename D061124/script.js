786441008e4dd5d25b69c0e27697984363dc8f0f
ola-oye <olasunkanmioyetunji4@gmail.com>
2024-11-06 10:57:26 +0100

    Algorithmic thinking with javascript

---
+++ D061124/script.js
@@ -0,0 +1,7 @@
+let counter = 0;
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 0) {
+        counter++;
+    }
+}
+console.log(`The number of even number between 0 to 100 is ${counter}`); // The number of even number between 0 to 100 is 50
